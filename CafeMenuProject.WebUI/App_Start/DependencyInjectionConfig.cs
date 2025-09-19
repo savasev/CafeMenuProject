2f5997eda107cebeb421fd3c72512ecfaebc3e46
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-19 17:57:32 +0300

    .

---
+++ CafeMenuProject.WebUI/App_Start/DependencyInjectionConfig.cs
@@ -0,0 +1,27 @@
+﻿using CafeMenuProject.Business.Abstract;
+using CafeMenuProject.Business.Concrete;
+using CafeMenuProject.DataAccess.Abstract;
+using CafeMenuProject.DataAccess.Concrete;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace CafeMenuProject.WebUI.App_Start
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var container = new UnityContainer();
+
+            // Repository Binding
+            container.RegisterType(typeof(IRepository<>), typeof(EfRepository<>));
+
+            // Service Binding
+            container.RegisterType<ICategoryService, CategoryService>();
+
+            // Controller'larda çözümleme
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
---
+++ CafeMenuProject.WebUI/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using CafeMenuProject.WebUI.App_Start;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace CafeMenuProject.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            DependencyInjectionConfig.RegisterDependencies();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
