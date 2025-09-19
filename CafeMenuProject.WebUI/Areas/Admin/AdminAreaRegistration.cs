8102a7fdc66b7872edf9154c37447db0fe3b6721
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-19 23:02:41 +0300

    .

---
+++ CafeMenuProject.WebUI/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace CafeMenuProject.WebUI.Areas.Admin
+{
+    /// <summary>
+    /// Admin area registration
+    /// </summary>
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Admin";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(name: "admin_default",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
