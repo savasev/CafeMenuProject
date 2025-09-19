e7736efa1c476b7aad5e48ceb69ff958f04dbebd
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-19 21:42:17 +0300

    .

---
+++ CafeMenuProject.Business/Abstract/IProductPropertyService.cs
@@ -0,0 +1,22 @@
+﻿using CafeMenuProject.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CafeMenuProject.Business.Abstract
+{
+    /// <summary>
+    /// Product property service interface
+    /// </summary>
+    public interface IProductPropertyService
+    {
+        Task<IList<ProductProperty>> GetAllProductPropertiesAsync();
+
+        Task<ProductProperty> GetProductPropertyByIdAsync(int id);
+
+        Task InsertProductPropertyAsync(ProductProperty productProperty);
+
+        Task UpdateProductPropertyAsync(ProductProperty productProperty);
+
+        Task DeleteProductPropertyAsync(ProductProperty productProperty);
+    }
+}
---
+++ CafeMenuProject.Business/Abstract/IPropertyService.cs
@@ -0,0 +1,22 @@
+﻿using CafeMenuProject.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CafeMenuProject.Business.Abstract
+{
+    /// <summary>
+    /// Property service interface
+    /// </summary>
+    public interface IPropertyService
+    {
+        Task<IList<Property>> GetAllPropertysAsync();
+
+        Task<Property> GetPropertyByIdAsync(int id);
+
+        Task InsertPropertyAsync(Property property);
+
+        Task UpdatePropertyAsync(Property property);
+
+        Task DeletePropertyAsync(Property property);
+    }
+}
