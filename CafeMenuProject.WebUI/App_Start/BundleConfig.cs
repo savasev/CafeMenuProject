7a9a62b5fedaaa1db83846ed39dc5b6627556e43
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-20 00:07:04 +0300

    .

--- CafeMenuProject.WebUI/App_Start/BundleConfig.cs
+++ CafeMenuProject.WebUI/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace CafeMenuProject.WebUI
 {
@@ -8,23 +7,34 @@ namespace CafeMenuProject.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/adminlte/css").Include(
+                "~/Content/adminlte/plugins/fontawesome-free/css/all.min.css",
+                "~/Content/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css",
+                "~/Content/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+                "~/Content/adminlte/plugins/jqvmap/jqvmap.min.css",
+                "~/Content/adminlte/dist/css/adminlte.min.css",
+                "~/Content/adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css",
+                "~/Content/adminlte/plugins/daterangepicker/daterangepicker.css",
+                "~/Content/adminlte/plugins/summernote/summernote-bs4.min.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/adminlte/js").Include(
+                "~/Content/adminlte/plugins/jquery/jquery.min.js",
+                "~/Content/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/adminlte/plugins/chart.js/Chart.min.js",
+                "~/Content/adminlte/plugins/sparklines/sparkline.js",
+                "~/Content/adminlte/plugins/jqvmap/jquery.vmap.min.js",
+                "~/Content/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js",
+                "~/Content/adminlte/plugins/jquery-knob/jquery.knob.min.js",
+                "~/Content/adminlte/plugins/moment/moment.min.js",
+                "~/Content/adminlte/plugins/daterangepicker/daterangepicker.js",
+                "~/Content/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js",
+                "~/Content/adminlte/plugins/summernote/summernote-bs4.min.js",
+                "~/Content/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
+                "~/Content/adminlte/dist/js/adminlte.js",
+                "~/Content/adminlte/dist/js/demo.js",
+                "~/Content/adminlte/dist/js/pages/dashboard.js"
+            ));
         }
     }
 }
