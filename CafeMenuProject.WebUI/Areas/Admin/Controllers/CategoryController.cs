393da59867239df3be40485a2d30838b86da5082
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-20 12:15:25 +0300

    .

--- CafeMenuProject.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ CafeMenuProject.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,8 @@
 ﻿using CafeMenuProject.Business.Abstract;
+using CafeMenuProject.WebUI.Areas.Admin.Models.Categories;
+using CafeMenuProject.WebUI.Areas.Admin.Models.Products;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace CafeMenuProject.WebUI.Areas.Admin.Controllers
@@ -16,6 +20,24 @@ namespace CafeMenuProject.WebUI.Areas.Admin.Controllers
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        private async Task<CreateCategoryModel> PrepareCreateCategoryModelAsync(CreateCategoryModel model = null)
+        {
+            if (model == null)
+                model = new CreateCategoryModel();
+
+            model.AvailableParentCategories = (await _categoryService.GetAllCategoriesAsync()).Select(x => new SelectListItem
+            {
+                Value = x.CategoryId.ToString(),
+                Text = x.CategoryName,
+            }).ToList();
+
+            return model;
         }
 
         #endregion
@@ -38,9 +60,11 @@ namespace CafeMenuProject.WebUI.Areas.Admin.Controllers
 
         #region Create
 
-        public ActionResult Create()
+        public async Task<ActionResult> Create()
         {
-            return View();
+            var model = await PrepareCreateCategoryModelAsync();
+
+            return View(model);
         }
 
         #endregion
--- CafeMenuProject.WebUI/Areas/Admin/Models/Categories/CreateCategoryModel.cs
+++ CafeMenuProject.WebUI/Areas/Admin/Models/Categories/CreateCategoryModel.cs
@@ -1,6 +1,27 @@
-﻿namespace CafeMenuProject.WebUI.Areas.Admin.Models.Categories
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CafeMenuProject.WebUI.Areas.Admin.Models.Categories
 {
     public class CreateCategoryModel
     {
+        #region Ctor
+
+        public CreateCategoryModel()
+        {
+            AvailableParentCategories = new List<SelectListItem>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string CategoryName { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        public IList<SelectListItem> AvailableParentCategories { get; set; }
+
+        #endregion
     }
 }
