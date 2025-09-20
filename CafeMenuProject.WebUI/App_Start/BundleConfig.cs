50230ec5dad875c1e8496ea09088f1dc7f8e391f
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-20 16:31:13 +0300

    .

--- CafeMenuProject.WebUI/App_Start/BundleConfig.cs
+++ CafeMenuProject.WebUI/App_Start/BundleConfig.cs
@@ -7,45 +7,47 @@ namespace CafeMenuProject.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            #region Admin area
+            #region Admin area bundles
 
             bundles.Add(new StyleBundle("~/bundles/admin/css").Include(
                 "~/Content/adminlte/plugins/fontawesome-free/css/all.min.css",
-                // "~/Content/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css",
                 "~/Content/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css",
-                // "~/Content/adminlte/plugins/jqvmap/jqvmap.min.css",
                 "~/Content/adminlte/dist/css/adminlte.min.css",
                 "~/Content/adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css",
-                // "~/Content/adminlte/plugins/daterangepicker/daterangepicker.css",
-                // "~/Content/adminlte/plugins/summernote/summernote-bs4.min.css",
                 "~/Content/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css",
                 "~/Content/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css"
-                //, "~/Content/adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css"
             ));
 
-            bundles.Add(new ScriptBundle("~/bundles/admin/js").Include(
-                "~/Content/adminlte/plugins/jquery/jquery.min.js",
+            bundles.Add(new ScriptBundle("~/bundles/admin/jquery").Include(
+                "~/Content/adminlte/plugins/jquery/jquery.min.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin/jquery-ui-overlay").Include(
                 "~/Content/adminlte/plugins/jquery-ui/jquery-ui.min.js",
-                "~/Content/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js",
-                // "~/Content/adminlte/plugins/chart.js/Chart.min.js",
-                // "~/Content/adminlte/plugins/sparklines/sparkline.js",
-                // "~/Content/adminlte/plugins/jqvmap/jquery.vmap.min.js",
-                // "~/Content/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js",
-                // "~/Content/adminlte/plugins/jquery-knob/jquery.knob.min.js",
-                "~/Content/adminlte/plugins/moment/moment.min.js",
-                // "~/Content/adminlte/plugins/daterangepicker/daterangepicker.js",
-                // "~/Content/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js",
-                // "~/Content/adminlte/plugins/summernote/summernote-bs4.min.js",
-                "~/Content/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
-                "~/Content/adminlte/dist/js/adminlte.js",
+                "~/Content/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin/bootstrap").Include(
+                "~/Content/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin/adminlte").Include(
+                "~/Content/adminlte/dist/js/adminlte.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin/datatables").Include(
                 "~/Content/adminlte/plugins/datatables/jquery.dataTables.min.js",
                 "~/Content/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js",
                 "~/Content/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"
             ));
 
+            bundles.Add(new ScriptBundle("~/bundles/admin/moment").Include(
+                "~/Content/adminlte/plugins/moment/moment.min.js"
+            ));
+            
             #endregion
 
-            #region Frontend
+            #region Other bundles
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
