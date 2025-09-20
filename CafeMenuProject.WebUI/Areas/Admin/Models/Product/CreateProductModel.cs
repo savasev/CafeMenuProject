47f06d4564a3b1cb6c93c607748d5726d884b926
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-20 23:46:24 +0300

    .

--- CafeMenuProject.WebUI/Areas/Admin/Models/Product/CreateProductModel.cs
+++ CafeMenuProject.WebUI/Areas/Admin/Models/Product/CreateProductModel.cs
@@ -1,6 +1,34 @@
-﻿namespace CafeMenuProject.WebUI.Areas.Admin.Models.Product
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CafeMenuProject.WebUI.Areas.Admin.Models.Product
 {
+    /// <summary>
+    /// Create product model
+    /// </summary>
     public class CreateProductModel
     {
+        #region Constructor
+
+        public CreateProductModel()
+        {
+            AvailableCategories = new List<SelectListItem>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string ProductName { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public IList<SelectListItem> AvailableCategories { get; set; }
+
+        #endregion
     }
 }
