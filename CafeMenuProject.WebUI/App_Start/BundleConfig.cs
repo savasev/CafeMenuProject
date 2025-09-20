76f0daa0850ad7200f11a9e0b14038e023b3a5e4
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-20 08:20:22 +0300

    .

--- CafeMenuProject.WebUI/App_Start/BundleConfig.cs
+++ CafeMenuProject.WebUI/App_Start/BundleConfig.cs
@@ -7,6 +7,8 @@ namespace CafeMenuProject.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region Admin area
+
             bundles.Add(new StyleBundle("~/bundles/adminlte/css").Include(
                 "~/Content/adminlte/plugins/fontawesome-free/css/all.min.css",
                 "~/Content/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css",
@@ -34,6 +36,30 @@ namespace CafeMenuProject.WebUI
                 "~/Content/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
                 "~/Content/adminlte/dist/js/adminlte.js"
             ));
+
+            #endregion
+
+            #region Frontend
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            #endregion
         }
     }
 }
--- CafeMenuProject.WebUI/App_Start/RouteConfig.cs
+++ CafeMenuProject.WebUI/App_Start/RouteConfig.cs
@@ -12,7 +12,8 @@ namespace CafeMenuProject.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "CafeMenuProject.WebUI.Controllers" }
             );
         }
     }
