15190e33729a8564d119009e78777e0609916420
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-21 11:28:50 +0300

    .

---
+++ CafeMenuProject.WebUI/Areas/Admin/Controllers/ComponentsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace CafeMenuProject.WebUI.Areas.Admin.Controllers
+{
+    public class ComponentsController : Controller
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Ctor
+
+        public ComponentsController()
+        {
+            
+        }
+
+        #endregion
+
+        #region Methods
+
+        [ChildActionOnly]
+        public ActionResult ProductProperyWidget(int productId)
+        {
+            return PartialView("_ProductProperyWidget");
+        }
+
+        #endregion
+    }
+}
