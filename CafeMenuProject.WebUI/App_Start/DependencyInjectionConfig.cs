4ed1222c917f8b14eb9cc10b4f525e02be2d3ffe
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-21 19:57:38 +0300

    .

--- CafeMenuProject.WebUI/App_Start/DependencyInjectionConfig.cs
+++ CafeMenuProject.WebUI/App_Start/DependencyInjectionConfig.cs
@@ -5,7 +5,6 @@ using CafeMenuProject.DataAccess.Concrete;
 using System.Web.Mvc;
 using Unity;
 using Unity.AspNet.Mvc;
-using Unity.Lifetime;
 
 namespace CafeMenuProject.WebUI.App_Start
 {
@@ -18,13 +17,13 @@ namespace CafeMenuProject.WebUI.App_Start
         {
             var container = new UnityContainer();
 
-            container.RegisterType(typeof(IRepository<>), typeof(EfRepository<>), new HierarchicalLifetimeManager());
+            container.RegisterType(typeof(IRepository<>), typeof(EfRepository<>));
 
-            container.RegisterType<IUserService, UserService>(new HierarchicalLifetimeManager());
-            container.RegisterType<ICategoryService, CategoryService>(new HierarchicalLifetimeManager());
-            container.RegisterType<IProductService, ProductService>(new HierarchicalLifetimeManager());
-            container.RegisterType<IPropertyService, PropertyService>(new HierarchicalLifetimeManager());
-            container.RegisterType<IProductPropertyService, ProductPropertyService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserService, UserService>();
+            container.RegisterType<ICategoryService, CategoryService>();
+            container.RegisterType<IProductService, ProductService>();
+            container.RegisterType<IPropertyService, PropertyService>();
+            container.RegisterType<IProductPropertyService, ProductPropertyService>();
 
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
