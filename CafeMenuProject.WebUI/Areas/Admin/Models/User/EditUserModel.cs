2cc29f976f295e6d4ef18c51bddc68f15fd366fb
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-21 13:00:04 +0300

    .

---
+++ CafeMenuProject.WebUI/Areas/Admin/Models/User/EditUserModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CafeMenuProject.WebUI.Areas.Admin.Models.User
+{
+    /// <summary>
+    /// Edit user model
+    /// </summary>
+    public class EditUserModel
+    {
+        #region Properties
+
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        #endregion
+    }
+}
